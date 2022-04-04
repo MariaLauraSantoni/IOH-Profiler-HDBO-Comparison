eb511c9d1198fa2c4524766da997e832687bc667
MariaLaura <marialaura.santoni@studenti.unicam.it>
2022-04-04 17:34:00 +0200

    script with class definitions

--- comp_HDBO.py
+++ comp_HDBO.py
@@ -7,14 +7,10 @@ import numpy as np
 
 # Set solver
 # Alternatives: "saasbo", "BO_sklearn", "BO_bayesoptim", "random", "RPA_BO", "linearPCABO"
-solver = "linearPCABO"
-
-import sys
-sys.path.append('./mylib/' + 'lib_' + solver)
-
-print(sys.path)
 
 
+alternatives = [ "saasbo", "BO_sklearn", "BO_bayesoptim", "random", "linearPCABO" ]
+#alternatives = ["random", "linearPCABO", "BO_sklearn"]
 
 
 
@@ -22,7 +18,7 @@ print(sys.path)
 dim = 10
 ub = 5
 lb = -5
-budget=20
+budget=10
 #budget= 10 * dim + 50
 DoE_samples = int(.20 * budget)
 
@@ -37,105 +33,117 @@ print(f.constraint.lb, f.constraint.ub)
 #Show the state of the optimization
 print(f.state)
 
-#Create default logger compatible with IOHanalyzer
-l = logger.Analyzer(root="data", folder_name="run", algorithm_name=solver, algorithm_info="test of IOHexperimenter in python")
-f.attach_logger(l)
 
+for item in alternatives :
 
-if __name__ == "__main__":
-    # # create an instance of this algorithm
-    # o = RandomSearch(1000)
+    solver = item
+
+    import sys
+
+    sys.path.append('./mylib/' + 'lib_' + solver)
+
+    print(sys.path)
+    #Create default logger compatible with IOHanalyzer
+    l = logger.Analyzer(root="data", folder_name="run", algorithm_name=solver, algorithm_info="test of IOHexperimenter in python")
     f.attach_logger(l)
-    # saasbo(f)
-    if solver == "saasbo":
-        # packages for saasbo
-        from saasbo import run_saasbo
-
-        run_saasbo(
-            f,
-            np.ones(dim)*ub,
-            np.ones(dim)*lb,
-            budget,
-            DoE_samples,
-            seed=42,
-            alpha=0.01,
-            num_warmup=256,
-            num_samples=256,
-            thinning=32,
-            device="cpu",
-        )
-    elif solver == "BO_sklearn":
-        # packages for BO_sklearn
-        from skopt import gp_minimize
-
-        gp_minimize(f,  # the function to minimize
-                    list((((lb, ub),) * dim)),  # the bounds on each dimension of x
-                    acq_func="EI",  # the acquisition function
-                    n_calls=budget,  # the number of evaluations of f
-                    n_random_starts=DoE_samples,  # the number of random initialization points
-                    noise=0.1 ** 2,  # the noise level (optional)
-                    random_state=1234)
-    elif solver == "BO_bayesoptim":
-        # packages for BO_bayesoptim
-        from bayes_optim import BO, RealSpace
-        from bayes_optim.surrogate import GaussianProcess
-
-        space = RealSpace([lb, ub]) * dim  # create the search space
-
-        # hyperparameters of the GPR model
-        thetaL = 1e-10 * (ub - lb) * np.ones(dim)
-        thetaU = 10 * (ub - lb) * np.ones(dim)
-        model = GaussianProcess(  # create the GPR model
-            thetaL=thetaL, thetaU=thetaU
-        )
-
-        opt = BO(
-            search_space=space,
-            obj_fun=f,
-            model=model,
-            DoE_size=DoE_samples,  # number of initial sample points
-            max_FEs=budget,  # maximal function evaluation
-            verbose=True
-        )
-        opt.run()
-    elif solver == "random":
-        # packages for random
-        import ioh
-        def random_search(problem: ioh.problem.Real, seed: int = 42, budget: int = None) -> ioh.RealSolution:
-            np.random.seed(seed)
-
-            if budget is None:
-                budget = int(problem.meta_data.n_variables * 1e4)
-
-            for _ in range(budget):
-                x = np.random.uniform(problem.constraint.lb, problem.constraint.ub)
-
-                # problem automatically tracks the current best search point
-                f = problem(x)
-
-            return problem.state.current_best
 
 
-        random_search(f, 42, budget)
+    if __name__ == "__main__":
+        # # create an instance of this algorithm
+        # o = RandomSearch(1000)
+        f.attach_logger(l)
+        # saasbo(f)
+        if solver == "saasbo":
+            # packages for saasbo
+            from saasbo import run_saasbo
+
+            run_saasbo(
+                f,
+                np.ones(dim)*ub,
+                np.ones(dim)*lb,
+                budget,
+                DoE_samples,
+                seed=42,
+                alpha=0.01,
+                num_warmup=256,
+                num_samples=256,
+                thinning=32,
+                device="cpu",
+            )
+
+        elif solver == "BO_sklearn":
+            # packages for BO_sklearn
+            from skopt import gp_minimize
+
+            gp_minimize(f,  # the function to minimize
+                        list((((lb, ub),) * dim)),  # the bounds on each dimension of x
+                        acq_func="EI",  # the acquisition function
+                        n_calls=budget,  # the number of evaluations of f
+                        n_random_starts=DoE_samples,  # the number of random initialization points
+                        noise=0.1 ** 2,  # the noise level (optional)
+                        random_state=1234)
+
+        elif solver == "BO_bayesoptim":
+            # packages for BO_bayesoptim
+            from bayes_optim import BO, RealSpace
+            from bayes_optim.surrogate import GaussianProcess
+
+            space = RealSpace([lb, ub]) * dim  # create the search space
+
+            # hyperparameters of the GPR model
+            thetaL = 1e-10 * (ub - lb) * np.ones(dim)
+            thetaU = 10 * (ub - lb) * np.ones(dim)
+            model = GaussianProcess(  # create the GPR model
+                thetaL=thetaL, thetaU=thetaU
+            )
+
+            opt = BO(
+                search_space=space,
+                obj_fun=f,
+                model=model,
+                DoE_size=DoE_samples,  # number of initial sample points
+                max_FEs=budget,  # maximal function evaluation
+                verbose=True
+            )
+            opt.run()
+
+        elif solver == "random":
+            # packages for random
+            import ioh
+            def random_search(problem: ioh.problem.Real, seed: int = 42, budget: int = None) -> ioh.RealSolution:
+                np.random.seed(seed)
+
+                if budget is None:
+                    budget = int(problem.meta_data.n_variables * 1e4)
+
+                for _ in range(budget):
+                    x = np.random.uniform(problem.constraint.lb, problem.constraint.ub)
+
+                    # problem automatically tracks the current best search point
+                    f = problem(x)
+
+                return problem.state.current_best
 
 
-    elif solver == "linearPCABO":
-        # packages for linearPCABO
-        sys.path.insert(0, "./mylib/lib_linearPCABO/Bayesian-Optimization")
-        from bayes_optim.extension import PCABO, RealSpace
+            random_search(f, 42, budget)
 
-        np.random.seed(123)
-        space = RealSpace([lb, ub]) * dim
-        opt = PCABO(
-            search_space=space,
-            obj_fun=f,
-            DoE_size= DoE_samples,
-            max_FEs=budget, # 10 * dim + 50,
-            verbose=True,
-            n_point=1,
-            n_components=0.95,
-            acquisition_optimization={"optimizer": "BFGS"},
-        )
-        print(opt.run())
+        elif solver == "linearPCABO":
+            # packages for linearPCABO
+            sys.path.insert(0, "./mylib/lib_linearPCABO/Bayesian-Optimization")
+            from bayes_optim.extension import PCABO, RealSpace
+
+            np.random.seed(123)
+            space = RealSpace([lb, ub]) * dim
+            opt = PCABO(
+                search_space=space,
+                obj_fun=f,
+                DoE_size= DoE_samples,
+                max_FEs=budget, # 10 * dim + 50,
+                verbose=True,
+                n_point=1,
+                n_components=0.95,
+                acquisition_optimization={"optimizer": "BFGS"},
+            )
+            print(opt.run())
 
 
---
+++ wrapper.py
@@ -0,0 +1,197 @@
+import numpy as np
+
+from ioh import get_problem, logger
+
+
+class SaasboWrapper:
+    def __init__(self, func, dim, ub, lb, total_budget, DoE_size, random_seed):
+        import sys
+        sys.path.append('./mylib/' + 'lib_' + "saasbo")
+        print(sys.path)
+
+        self.func = func
+        self.dim = dim
+        self.ub = ub
+        self.lb = lb
+        self.total_budget = total_budget
+        self.Doe_size = DoE_size
+        self.random_seed = random_seed
+
+    def run(self):
+        from saasbo import run_saasbo
+        run_saasbo(
+            self.func,
+            np.ones(self.dim) * self.ub,
+            np.ones(self.dim) * self.lb,
+            self.total_budget,
+            self.Doe_size,
+            self.random_seed,
+            alpha=0.01,
+            num_warmup=256,
+            num_samples=256,
+            thinning=32,
+            device="cpu",
+        )
+
+class BO_sklearnWrapper:
+    def __init__(self, func, dim, ub, lb, total_budget, DoE_size, random_seed):
+        import sys
+        sys.path.append('./mylib/' + 'lib_' + "BO_sklearn")
+        print(sys.path)
+
+        self.func = func
+        self.dim = dim
+        self.ub = ub
+        self.lb = lb
+        self.total_budget = total_budget
+        self.Doe_size = DoE_size
+        self.random_seed = random_seed
+
+    def run(self):
+        from skopt import gp_minimize
+
+        gp_minimize(self.func,  # the function to minimize
+                    list((((self.lb, self.ub),) * self.dim)),  # the bounds on each dimension of x
+                    acq_func="EI",  # the acquisition function
+                    n_calls=self.total_budget,  # the number of evaluations of f
+                    n_random_starts=self.Doe_size,  # the number of random initialization points
+                    noise=0.1 ** 2,  # the noise level (optional)
+                    random_state=1234)
+
+class BO_bayesoptimWrapper:
+    def __init__(self, func, dim, ub, lb, total_budget, DoE_size, random_seed):
+        import sys
+        sys.path.append('./mylib/' + 'lib_' + "BO_bayesoptim")
+        print(sys.path)
+
+        self.func = func
+        self.dim = dim
+        self.ub = ub
+        self.lb = lb
+        self.total_budget = total_budget
+        self.Doe_size = DoE_size
+        self.random_seed = random_seed
+
+    def run(self):
+        from bayes_optim import BO, RealSpace
+        from bayes_optim.surrogate import GaussianProcess
+
+        space = RealSpace([self.lb, self.ub]) * self.dim  # create the search space
+
+        # hyperparameters of the GPR model
+        thetaL = 1e-10 * (self.ub - self.lb) * np.ones(self.dim)
+        thetaU = 10 * (self.ub - self.lb) * np.ones(self.dim)
+        model = GaussianProcess(  # create the GPR model
+            thetaL=thetaL, thetaU=thetaU
+        )
+
+        opt = BO(
+            search_space=space,
+            obj_fun=self.func,
+            model=model,
+            DoE_size=self.Doe_size,  # number of initial sample points
+            max_FEs=self.total_budget,  # maximal function evaluation
+            verbose=True
+        )
+        opt.run()
+
+class randomWrapper:
+    def __init__(self, func, dim, ub, lb, total_budget, DoE_size, random_seed):
+
+        self.func = func
+        self.dim = dim
+        self.ub = ub
+        self.lb = lb
+        self.total_budget = total_budget
+        self.Doe_size = DoE_size
+        self.random_seed = random_seed
+
+    def run(self):
+        import ioh
+        def random_search(problem: ioh.problem.Real, seed: int = 42, budget: int = None) -> ioh.RealSolution:
+            np.random.seed(seed)
+
+            if budget is None:
+                budget = int(problem.meta_data.n_variables * 1e4)
+
+            for _ in range(budget):
+                x = np.random.uniform(problem.constraint.lb, problem.constraint.ub)
+
+                # problem automatically tracks the current best search point
+                f = problem(x)
+
+            return problem.state.current_best
+
+        random_search(self.func, self.random_seed, self.total_budget)
+
+class linearPCABOWrapper:
+    def __init__(self, func, dim, ub, lb, total_budget, DoE_size, random_seed):
+        import sys
+        sys.path.append('./mylib/' + 'lib_' + "linearPCABO")
+        print(sys.path)
+
+        self.func = func
+        self.dim = dim
+        self.ub = ub
+        self.lb = lb
+        self.total_budget = total_budget
+        self.Doe_size = DoE_size
+        self.random_seed = random_seed
+
+    def run(self):
+        import sys
+        sys.path.insert(0, "./mylib/lib_linearPCABO/Bayesian-Optimization")
+        from bayes_optim.extension import PCABO, RealSpace
+
+        np.random.seed(123)
+        space = RealSpace([self.lb, self.ub]) * self.dim
+        opt = PCABO(
+            search_space=space,
+            obj_fun=self.func,
+            DoE_size=self.Doe_size,
+            max_FEs=self.total_budget,  # 10 * dim + 50,
+            verbose=True,
+            n_point=1,
+            n_components=0.95,
+            acquisition_optimization={"optimizer": "BFGS"},
+        )
+        print(opt.run())
+
+
+
+def marialaura(optimizer_name, func, ml_dim, ml_total_budget, ml_DoE_size, random_seed):
+    ub = +5
+    lb = -5
+    if optimizer_name == "saasbo":
+        solver = SaasboWrapper(func=func, dim=ml_dim, ub=ub, lb=lb, total_budget=ml_total_budget, DoE_size=ml_DoE_size,
+                               random_seed=random_seed)
+        solver.run()
+    if optimizer_name == "BO_sklearn":
+        solver = BO_sklearnWrapper(func=func, dim=ml_dim, ub=ub, lb=lb, total_budget=ml_total_budget, DoE_size=ml_DoE_size,
+                               random_seed=random_seed)
+        solver.run()
+    if optimizer_name == "BO_bayesoptim":
+        solver = BO_bayesoptimWrapper(func=func, dim=ml_dim, ub=ub, lb=lb, total_budget=ml_total_budget, DoE_size=ml_DoE_size,
+                               random_seed=random_seed)
+        solver.run()
+    if optimizer_name == "random":
+        solver = randomWrapper(func=func, dim=ml_dim, ub=ub, lb=lb, total_budget=ml_total_budget, DoE_size=ml_DoE_size,
+                               random_seed=random_seed)
+        solver.run()
+    if optimizer_name == "linearPCABO":
+        solver = randomWrapper(func=func, dim=ml_dim, ub=ub, lb=lb, total_budget=ml_total_budget, DoE_size=ml_DoE_size,
+                               random_seed=random_seed)
+        solver.run()
+
+
+if __name__ == "__main__":
+    dim = 10
+    total_budget = 20
+    doe_size = 5
+    seed = 0
+    # Algorithm alternatives:
+    algorithm_name = "linearPCABO"
+
+    f = get_problem(21, dimension=dim, instance=1, problem_type='Real')
+
+    marialaura(algorithm_name, f, dim, total_budget, doe_size, seed)
